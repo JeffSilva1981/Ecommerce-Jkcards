0ef47e19ac79747f0ca3cad107865cce41048627
JeffSilva1981 <jeff.contactwork@gmail.com>
2026-05-05 09:19:57 -0300

    Implemented Order and OrderStatus

--- src/main/java/com/jeffsilva/jkcards/entities/User.java
+++ src/main/java/com/jeffsilva/jkcards/entities/User.java
@@ -3,6 +3,8 @@ package com.jeffsilva.jkcards.entities;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "tb_user")
@@ -19,6 +21,9 @@ public class User {
     private LocalDate birthDate;
 
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User(){
 
@@ -79,5 +84,9 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 }
