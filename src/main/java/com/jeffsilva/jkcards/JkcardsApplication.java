15b33defba780d72bb45f42c5a1a2fd61d3bf463
JeffSilva1981 <jeff.contactwork@gmail.com>
2026-05-05 08:38:38 -0300

    First commit

---
+++ src/main/java/com/jeffsilva/jkcards/JkcardsApplication.java
@@ -0,0 +1,14 @@
+package com.jeffsilva.jkcards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JkcardsApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(JkcardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeffsilva/jkcards/JkcardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeffsilva.jkcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JkcardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
