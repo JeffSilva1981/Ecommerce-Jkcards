e8e8bea344412b480a89eb4fdbeb3f3d5b6e274b
JeffSilva1981 <jeff.contactwork@gmail.com>
2026-06-01 11:03:00 -0300

    Added Role, UserDetailsProjection, UserRepository and UserService

---
+++ src/main/java/com/jeffsilva/jkcards/entities/Role.java
@@ -0,0 +1,41 @@
+package com.jeffsilva.jkcards.entities;
+
+import jakarta.persistence.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+@Table(name = "tb_role")
+public class Role implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String authority;
+
+    public Role(){
+
+    }
+
+    public Role(Long id, String authority) {
+        this.id = id;
+        this.authority = authority;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Autowired
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+}
---
+++ src/main/java/com/jeffsilva/jkcards/projections/UserDetailsProjection.java
@@ -0,0 +1,9 @@
+package com.jeffsilva.jkcards.projections;
+
+public interface UserDetailsProjection {
+
+    String getUsername();
+    String getPassword();
+    Long getRoleId();
+    String getAuthority();
+}
