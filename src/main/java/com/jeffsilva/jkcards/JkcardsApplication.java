3ae892090c8f83ee40655b57849b357a1246c9f0
JeffSilva1981 <jeff.contactwork@gmail.com>
2026-06-01 18:29:21 -0300

    OAuth2, JWT and passaword grant

--- src/main/java/com/jeffsilva/jkcards/JkcardsApplication.java
+++ src/main/java/com/jeffsilva/jkcards/JkcardsApplication.java
@@ -1,14 +1,24 @@
 package com.jeffsilva.jkcards;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class JkcardsApplication {
+public class JkcardsApplication implements CommandLineRunner {
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(JkcardsApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Encode:" + passwordEncoder.encode( "123456"));
+	}
 }
